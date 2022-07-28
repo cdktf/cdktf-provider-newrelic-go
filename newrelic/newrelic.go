1d1cbb8529931309c53a6732445eab6528fc5638
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-07-28 15:10:08 +0000

    chore(release): v2.0.5

--- newrelic/newrelic.go
+++ newrelic/newrelic.go
@@ -795,7 +795,7 @@ type AlertChannelConfig struct {
 	//
 	// Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/newrelic/r/alert_channel#name AlertChannel#name}
 	Name *string `field:"required" json:"name" yaml:"name"`
-	// (Required) The type of channel. One of: (slack, user, victorops, webhook, email, opsgenie, pagerduty).
+	// (Required) The type of channel. One of: (user, victorops, webhook, email, opsgenie, pagerduty, slack).
 	//
 	// Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/newrelic/r/alert_channel#type AlertChannel#type}
 	Type *string `field:"required" json:"type" yaml:"type"`
@@ -3255,7 +3255,7 @@ type AlertConditionConfig struct {
 	//
 	// Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/newrelic/r/alert_condition#term AlertCondition#term}
 	Term interface{} `field:"required" json:"term" yaml:"term"`
-	// The type of condition. One of: (apm_app_metric, apm_jvm_metric, apm_kt_metric, browser_metric, mobile_metric, servers_metric).
+	// The type of condition. One of: (mobile_metric, servers_metric, apm_app_metric, apm_jvm_metric, apm_kt_metric, browser_metric).
 	//
 	// Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/newrelic/r/alert_condition#type AlertCondition#type}
 	Type *string `field:"required" json:"type" yaml:"type"`
