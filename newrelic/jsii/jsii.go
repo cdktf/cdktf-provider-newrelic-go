e79af0b997d47fad928b1e7b829dc684cb7968d9
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-09-20 04:01:43 +0000

    chore(release): v2.0.114

--- newrelic/jsii/jsii.go
+++ newrelic/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	cdktf      "github.com/hashicorp/terraform-cdk-go/cdktf/jsii"
 )
 
-//go:embed cdktf-provider-newrelic-2.0.113.tgz
+//go:embed cdktf-provider-newrelic-2.0.114.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@cdktf/provider-newrelic", "2.0.113", tarball)
+	_jsii_.Load("@cdktf/provider-newrelic", "2.0.114", tarball)
 }
