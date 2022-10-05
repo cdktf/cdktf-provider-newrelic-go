b882d6de0e066df63bec1a5bbfcdaa8113f2a83e
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:29:29 +0000

    chore(release): v3.0.3

--- newrelic/insightsevent/insightsevent_InsightsEventEventAttributeList.go
+++ newrelic/insightsevent/insightsevent_InsightsEventEventAttributeList.go
@@ -2,9 +2,9 @@ package insightsevent
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-newrelic-go/newrelic/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-newrelic-go/newrelic/v3/jsii"
 
-	"github.com/hashicorp/cdktf-provider-newrelic-go/newrelic/v3/insightsevent/internal"
+	"github.com/cdktf/cdktf-provider-newrelic-go/newrelic/v3/insightsevent/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
