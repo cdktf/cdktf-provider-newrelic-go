8fa4a83e6dbc67a4ec3acbb5f220917332db03b6
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-08-17 03:18:32 +0000

    chore(release): v12.12.2

--- newrelic/cloudawsintegrations/CloudAwsIntegrationsVpc.go
+++ newrelic/cloudawsintegrations/CloudAwsIntegrationsVpc.go
@@ -7,31 +7,31 @@ package cloudawsintegrations
 type CloudAwsIntegrationsVpc struct {
 	// Specify each AWS region that includes the resources that you want to monitor.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/cloud_aws_integrations#aws_regions CloudAwsIntegrations#aws_regions}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/cloud_aws_integrations#aws_regions CloudAwsIntegrations#aws_regions}
 	AwsRegions *[]*string `field:"optional" json:"awsRegions" yaml:"awsRegions"`
 	// Specify if NAT gateway should be monitored.
 	//
 	// May affect total data collection time and contribute to the Cloud provider API rate limit.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/cloud_aws_integrations#fetch_nat_gateway CloudAwsIntegrations#fetch_nat_gateway}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/cloud_aws_integrations#fetch_nat_gateway CloudAwsIntegrations#fetch_nat_gateway}
 	FetchNatGateway interface{} `field:"optional" json:"fetchNatGateway" yaml:"fetchNatGateway"`
 	// Specify if VPN should be monitored.
 	//
 	// May affect total data collection time and contribute to the Cloud provider API rate limit.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/cloud_aws_integrations#fetch_vpn CloudAwsIntegrations#fetch_vpn}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/cloud_aws_integrations#fetch_vpn CloudAwsIntegrations#fetch_vpn}
 	FetchVpn interface{} `field:"optional" json:"fetchVpn" yaml:"fetchVpn"`
 	// The data polling interval in seconds.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/cloud_aws_integrations#metrics_polling_interval CloudAwsIntegrations#metrics_polling_interval}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/cloud_aws_integrations#metrics_polling_interval CloudAwsIntegrations#metrics_polling_interval}
 	MetricsPollingInterval *float64 `field:"optional" json:"metricsPollingInterval" yaml:"metricsPollingInterval"`
 	// Specify a Tag key associated with the resources that you want to monitor. Filter values are case-sensitive.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/cloud_aws_integrations#tag_key CloudAwsIntegrations#tag_key}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/cloud_aws_integrations#tag_key CloudAwsIntegrations#tag_key}
 	TagKey *string `field:"optional" json:"tagKey" yaml:"tagKey"`
 	// Specify a Tag value associated with the resources that you want to monitor. Filter values are case-sensitive.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/cloud_aws_integrations#tag_value CloudAwsIntegrations#tag_value}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/cloud_aws_integrations#tag_value CloudAwsIntegrations#tag_value}
 	TagValue *string `field:"optional" json:"tagValue" yaml:"tagValue"`
 }
 
--- newrelic/syntheticsstepmonitor/SyntheticsStepMonitorSteps.go
+++ newrelic/syntheticsstepmonitor/SyntheticsStepMonitorSteps.go
@@ -7,15 +7,15 @@ package syntheticsstepmonitor
 type SyntheticsStepMonitorSteps struct {
 	// The position of the step within the script ranging from 0-100.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/synthetics_step_monitor#ordinal SyntheticsStepMonitor#ordinal}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/synthetics_step_monitor#ordinal SyntheticsStepMonitor#ordinal}
 	Ordinal *float64 `field:"required" json:"ordinal" yaml:"ordinal"`
 	// The type of step to be added to the script.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/synthetics_step_monitor#type SyntheticsStepMonitor#type}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/synthetics_step_monitor#type SyntheticsStepMonitor#type}
 	Type *string `field:"required" json:"type" yaml:"type"`
 	// The metadata values related to the check the step performs.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.1/docs/resources/synthetics_step_monitor#values SyntheticsStepMonitor#values}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.42.3/docs/resources/synthetics_step_monitor#values SyntheticsStepMonitor#values}
 	Values *[]*string `field:"optional" json:"values" yaml:"values"`
 }
 
