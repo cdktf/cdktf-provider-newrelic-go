7be48019b13bf78fd0d519ff2f3249908257b490
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-03-06 03:27:23 +0000

    chore(release): v12.26.1

--- newrelic/logparsingrule/LogParsingRuleConfig.go
+++ newrelic/logparsingrule/LogParsingRuleConfig.go
@@ -24,42 +24,42 @@ type LogParsingRuleConfig struct {
 	Provisioners *[]interface{} `field:"optional" json:"provisioners" yaml:"provisioners"`
 	// Whether or not this rule is enabled.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#enabled LogParsingRule#enabled}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#enabled LogParsingRule#enabled}
 	Enabled interface{} `field:"required" json:"enabled" yaml:"enabled"`
 	// The Grok of what to parse.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#grok LogParsingRule#grok}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#grok LogParsingRule#grok}
 	Grok *string `field:"required" json:"grok" yaml:"grok"`
 	// The Lucene to match events to the parsing rule.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#lucene LogParsingRule#lucene}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#lucene LogParsingRule#lucene}
 	Lucene *string `field:"required" json:"lucene" yaml:"lucene"`
 	// A description of what this parsing rule represents.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#name LogParsingRule#name}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#name LogParsingRule#name}
 	Name *string `field:"required" json:"name" yaml:"name"`
 	// The NRQL to match events to the parsing rule.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#nrql LogParsingRule#nrql}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#nrql LogParsingRule#nrql}
 	Nrql *string `field:"required" json:"nrql" yaml:"nrql"`
 	// The account id associated with the obfuscation expression.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#account_id LogParsingRule#account_id}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#account_id LogParsingRule#account_id}
 	AccountId *float64 `field:"optional" json:"accountId" yaml:"accountId"`
 	// The parsing rule will apply to value of this attribute.
 	//
 	// If field is not provided, value will default to message.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#attribute LogParsingRule#attribute}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#attribute LogParsingRule#attribute}
 	Attribute *string `field:"optional" json:"attribute" yaml:"attribute"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#id LogParsingRule#id}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#id LogParsingRule#id}.
 	//
 	// Please be aware that the id field is automatically added to all resources in Terraform providers using a Terraform provider SDK version below 2.
 	// If you experience problems setting this value it might not be settable. Please take a look at the provider documentation to ensure it should be settable.
 	Id *string `field:"optional" json:"id" yaml:"id"`
 	// Whether the Grok pattern matched.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/log_parsing_rule#matched LogParsingRule#matched}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/log_parsing_rule#matched LogParsingRule#matched}
 	Matched interface{} `field:"optional" json:"matched" yaml:"matched"`
 }
 
--- newrelic/nrqlalertcondition/NrqlAlertConditionWarning.go
+++ newrelic/nrqlalertcondition/NrqlAlertConditionWarning.go
@@ -7,35 +7,35 @@ package nrqlalertcondition
 type NrqlAlertConditionWarning struct {
 	// For baseline conditions must be in range [1, 1000].
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/nrql_alert_condition#threshold NrqlAlertCondition#threshold}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/nrql_alert_condition#threshold NrqlAlertCondition#threshold}
 	Threshold *float64 `field:"required" json:"threshold" yaml:"threshold"`
 	// In minutes, must be in the range of 1 to 120 (inclusive).
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/nrql_alert_condition#duration NrqlAlertCondition#duration}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/nrql_alert_condition#duration NrqlAlertCondition#duration}
 	Duration *float64 `field:"optional" json:"duration" yaml:"duration"`
 	// One of (above, above_or_equals, below, below_or_equals, equals, not_equals). Defaults to 'equals'.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/nrql_alert_condition#operator NrqlAlertCondition#operator}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/nrql_alert_condition#operator NrqlAlertCondition#operator}
 	Operator *string `field:"optional" json:"operator" yaml:"operator"`
 	// prediction block.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/nrql_alert_condition#prediction NrqlAlertCondition#prediction}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/nrql_alert_condition#prediction NrqlAlertCondition#prediction}
 	Prediction *NrqlAlertConditionWarningPrediction `field:"optional" json:"prediction" yaml:"prediction"`
 	// The duration, in seconds, that the threshold must violate in order to create an incident.
 	//
 	// Value must be a multiple of the 'aggregation_window' (which has a default of 60 seconds). Value must be within 120-86400 seconds for baseline conditions, and within 60-86400 seconds for static conditions
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/nrql_alert_condition#threshold_duration NrqlAlertCondition#threshold_duration}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/nrql_alert_condition#threshold_duration NrqlAlertCondition#threshold_duration}
 	ThresholdDuration *float64 `field:"optional" json:"thresholdDuration" yaml:"thresholdDuration"`
 	// The criteria for how many data points must be in violation for the specified threshold duration.
 	//
 	// Valid values are: 'ALL' or 'AT_LEAST_ONCE' (case insensitive).
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/nrql_alert_condition#threshold_occurrences NrqlAlertCondition#threshold_occurrences}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/nrql_alert_condition#threshold_occurrences NrqlAlertCondition#threshold_occurrences}
 	ThresholdOccurrences *string `field:"optional" json:"thresholdOccurrences" yaml:"thresholdOccurrences"`
 	// Valid values are: 'all' or 'any'.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/nrql_alert_condition#time_function NrqlAlertCondition#time_function}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/nrql_alert_condition#time_function NrqlAlertCondition#time_function}
 	TimeFunction *string `field:"optional" json:"timeFunction" yaml:"timeFunction"`
 }
 
